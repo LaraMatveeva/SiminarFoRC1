ff16aa0e354971019b2ef95f6c79d75be26d07b8
LaraMatveeva <matveeva_l@mail.com>
2022-11-15 19:44:15 +0300

    Completed Task02,Task04,Task06,Task08

---
+++ Task02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите число 1:");
+int number1 = Convert.ToInt32(Console.ReadLine()); //конверт строку в число1
+
+Console.WriteLine("Введите число 2:");
+int number2 = Convert.ToInt32(Console.ReadLine()); //конверт строку в число2
+
+if (number1 > number2)Console.WriteLine($"max = {number1}");
+Console.WriteLine($"max = {number2}");
+
---
+++ Task08/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number <= 1)
+{ 
+    Console.WriteLine("Введите другое число больше 1:");
+    number = Convert.ToInt32(Console.ReadLine()); 
+}
+int count = 2;
+while (count <= number)
+{
+   Console.WriteLine(count);
+   count = count + 2; 
+}
